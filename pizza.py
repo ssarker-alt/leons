b0915972f88db1cfd7936fc0a3c0d53c6880b42b
ssarker-alt <suporna917@gmail.com>
2024-09-27 19:45:31 -0400

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,12 @@
+#Variables
+favorite_food = "Pizza"
+quantity = 3
+price = 12.99
+
+print("Before:", favorite_food, quantity, price)
+
+#Change
+favorite_food = "Sushi"
+
+# Print after change
+print("After:", favorite_food, quantity, price)
